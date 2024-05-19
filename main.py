dee594aef31e00ac571816664d5de85184245c51
mszmyd <erilf9@gmail.com>
2024-05-19 14:50:37 +0200

    delete dataset loading

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 
-from file_manager.data_manager import DataManager
 from model.efficentnetv2b0 import EfficientNetV2B0Model
 from model.mobilenetv2 import MobilenetV2Model
 from model.resnet50v2 import Resnet50Model
@@ -12,11 +11,6 @@ parser.add_argument('--model', type=str, choices=[
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    data_manager = DataManager()
-    data_manager.download_data()
-    data_manager.unzip_data("archive.zip", "original_dataset")
-    data_manager.resize_dataset(shape=(96, 96), source="original_dataset")
-
     if args.model == 'resnet50':
         model = Resnet50Model()
     elif args.model == 'efficientnet':
